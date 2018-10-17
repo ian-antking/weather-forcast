f1fe21174873c3f724236fe46ce6596a42fe0c0d
Ian King <ian.antking@gmail.com>
2018-10-17 21:14:50 +0100

    added test for rendering temperature

--- __tests__/components/forecast-summary.test.jsx
+++ __tests__/components/forecast-summary.test.jsx
@@ -21,3 +21,8 @@ it('renders the date', () => {
   const date = wrapper.find('div.forecast-summary__date').text();
   expect(date).toEqual('mockDate');
 });
+
+it('renders the temperature', () => {
+  const temperature = wrapper.find('div.forecaset-summary__temperature').text();
+  expect(temperature).toEqual('mockTemperature');
+});
