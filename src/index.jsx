93e203be0a73f67fe55b315a3eefa6d4a067c6ad
Ian King <ian.antking@gmail.com>
2018-10-19 18:29:06 +0100

    Tested amount of rendered components inside the forecast summaries component

--- src/index.jsx
+++ src/index.jsx
@@ -6,9 +6,12 @@ import { location, forecasts } from './data/forecast.json';
 import LocationDetails from './components/location-details';
 import ForecastSummaries from './components/forecast-summaries';
 
+import '../src/styles/app.scss';
+import '../src/styles/forecast-summaries.scss';
+
 const App = (props) => {
   return (
-    <div>
+    <div className="forecast">
       <LocationDetails location={props.location} />
       <ForecastSummaries forecasts={props.forecasts} />
     </div>
@@ -23,4 +26,4 @@ App.propTypes = {
   forecasts: PropTypes.array.isRequired,
 };
 
-render(<App location={location} forecasts={forecasts}/>, document.getElementById('root'));
+render(<App location={location} forecasts={forecasts} />, document.getElementById('root'));
