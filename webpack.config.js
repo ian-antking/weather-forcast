59f3bd2e962a540936e64498e1678af8a8259878
Ian King <ian.antking@gmail.com>
2018-12-06 13:41:13 +0000

    minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   target: 'web',
-  mode: 'development',
+  mode: process.env.BUILD_ENV === 'production' ? 'production' : 'development',
   entry: path.join(__dirname, 'src/index.jsx'),
   output: {
     path: path.resolve(__dirname, 'public'),
